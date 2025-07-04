af43b6f5c227225dff519babc2c17635ff622838
karolgle <karol.glesner@gmail.com>
2025-07-04 14:47:55 +0200

    Add simple web GUI for producer-consumer demo

--- src/main/java/com/example/producerconsumer/ProducerConsumerApplication.java
+++ src/main/java/com/example/producerconsumer/ProducerConsumerApplication.java
@@ -1,49 +1,12 @@
 package com.example.producerconsumer;
 
-import com.example.producerconsumer.interfaces.QueueConsumer;
-import com.example.producerconsumer.interfaces.QueueProducer;
-import com.example.producerconsumer.interfaces.TaskDataGenerator;
-import com.example.producerconsumer.services.ProducersConsumersService;
-import org.javatuples.Triplet;
-import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-
-import java.util.List;
 
 @SpringBootApplication
-public class ProducerConsumerApplication implements ApplicationContextAware {
-
-    private static int numberOfProducers;
-    private static int numberOfConsumers;
-
-    private static ApplicationContext applicationContext;
-
-    @Value("${producers.number:2}")
-    public void setNumberOfProducers(int numberOfProducers) {
-        ProducerConsumerApplication.numberOfProducers = numberOfProducers;
-    }
-
-    @Value("${consumers.number:4}")
-    public void setNumberOfConsumers(int numberOfConsumers) {
-        ProducerConsumerApplication.numberOfConsumers = numberOfConsumers;
-    }
+public class ProducerConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerConsumerApplication.class, args);
-
-        ProducersConsumersService producersConsumersService = applicationContext.getBean(ProducersConsumersService.class);
-        TaskDataGenerator<String> mathTaskDataGenerator = applicationContext.getBean(TaskDataGenerator.class);
-        Triplet<List<QueueProducer<String>>, List<QueueConsumer<String>>, List<String>> producersAndConsumers = producersConsumersService.prepareProducersAndConsumers(numberOfProducers, numberOfConsumers, mathTaskDataGenerator);
-
-        producersConsumersService.run(producersAndConsumers.getValue0(), producersAndConsumers.getValue1());
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
     }
 }
--- src/main/java/com/example/producerconsumer/QueueVisualizer.java
+++ src/main/java/com/example/producerconsumer/QueueVisualizer.java
@@ -2,20 +2,34 @@ package com.example.producerconsumer;
 
 import me.tongfei.progressbar.ProgressBar;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Simple helper that visualizes the current queue size using a progress bar.
  */
 public class QueueVisualizer {
 
     private final ProgressBar progressBar;
+    private final AtomicInteger currentSize = new AtomicInteger();
+    private final int capacity;
 
     public QueueVisualizer(int capacity) {
+        this.capacity = capacity;
         this.progressBar = new ProgressBar("Queue", capacity);
         this.progressBar.stepTo(0);
     }
 
     public synchronized void update(int currentSize) {
+        this.currentSize.set(currentSize);
         progressBar.stepTo(currentSize);
+    }
+
+    public int getCurrentSize() {
+        return currentSize.get();
+    }
+
+    public int getCapacity() {
+        return capacity;
     }
 
     public synchronized void close() {
